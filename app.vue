b59f670c10b1139b73030c18765d4562583d4210
Soh Mitian <pix4soh@gmail.com>
2023-04-10 23:40:58 +0900

    Update app.vue

--- app.vue
+++ app.vue
@@ -21,28 +21,3 @@
     },
   });
   </script>
-  
-
-
-
-<!-- <template>
-    <baklava-editor :plugin="viewPlugin"></baklava-editor>
-</template>
-
-<script>
-import { Editor } from "@baklavajs/core";
-import { ViewPlugin } from "@baklavajs/plugin-renderer-vue3";
-
-export default {
-    data() {
-        return {
-            editor: new Editor(),
-            viewPlugin: new ViewPlugin()
-        }
-    },
-    created() {
-        this.editor.use(this.viewPlugin);
-        // register your nodes, node options, node interface types, ...
-    }
-}
-</script> -->
